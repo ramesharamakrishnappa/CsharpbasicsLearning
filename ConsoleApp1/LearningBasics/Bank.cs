231a1c20d22f448471c853d287d4150353391376
Ramesharamakrishnappa <rameshar.225@gmail.com>
2021-05-27 09:55:10 +0530

    message

---
+++ ConsoleApp1/LearningBasics/Bank.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2.LearningBasics
+{
+
+    public class Bank
+
+    {
+        private int accountNumber = 1000000000;
+        public string customerName;
+        private double balance;
+
+
+        public int AccountNumber
+        {
+            get
+            {
+                // accountNumber= accountNumber + 1;
+                return accountNumber;
+            }
+        }
+
+        public double Balance
+        {
+            get
+            {
+                return balance;
+            }
+
+        }
+
+        public void WithdrawAmmount(double amount)
+        {
+
+            double tempbalance = balance - amount;
+            if (tempbalance >= 0)
+            {
+                balance = balance - amount;
+                Console.WriteLine($"Rs {amount} debited from your a/c {AccountNumber} and the current balance is Rs {balance}");
+            }
+            else
+                Console.WriteLine($"you do not have sufficient balance to withdraw mentioned amount Rs {amount}");
+
+        }
+
+        public void Deposit(double amount)
+        {
+            if (amount > 0)
+            {
+                balance = balance + amount;
+                Console.WriteLine($"Deposit of Rs {amount} to the account number {AccountNumber} is successfull and your balance is {balance}");
+            }
+            else
+                Console.WriteLine("please enter valid amount which should be greater than Zero(0)");
+        }
+
+    }
+
+
+}
---
+++ ConsoleApp1/LearningBasics/Customer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2.LearningBasics
+{
+    public class Customer
+    {
+        static void Main(string[] args)
+        {
+            Bank bank = new Bank();
+            bank.customerName = "ramesha";
+            bank.Deposit(50000);
+            bank.WithdrawAmmount(6000);
+            bank.WithdrawAmmount(44000);
+            bank.WithdrawAmmount(40000);
+            bank.Deposit(50000);
+            //Console.WriteLine($"Your balance is {bank.Balance} ");
+        }
+
+    }
+}
